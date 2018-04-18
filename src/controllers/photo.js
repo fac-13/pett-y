7d035e8485b16fe554e5d006ec23a0e625b035d5
tspeed90 <tspeed90@gmail.com>
2018-04-18 18:04:19 +0100

    removes console logs and cleans up code

--- src/controllers/photo.js
+++ src/controllers/photo.js
@@ -2,8 +2,6 @@ const photoList = ['key', 'value', 'asdf'];
 
 exports.get = (req, res, next) => {
   const { photo } = req.params;
-  console.log(req.params);
-  console.log('is photo undefined? photo controller', photo);
   if (photoList.includes(photo)) {
     return res.render('photo', { photo });
   }
