4f9818b50719aa59dfa44faeee2e0b13f791bdb5
tdoran <tomgdoran@gmail.com>
2018-04-18 15:09:43 +0100

    fixed typo

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -13,7 +13,7 @@ const sql = file => QueryFile(path.join(__dirname, file), { minify: true });
 const build = sql('./db_build.sql');
 
 const runDbBuild = (callback) => {
-  db
+  dbConnect
     .query(build)
     .then(res => {
       console.log('res', res);
