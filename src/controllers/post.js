497a284d05dd29de71cc92317a7fa2f3e86b8d51
tdoran <tomgdoran@gmail.com>
2018-04-20 11:38:31 +0100

    included next in routes for error route direction Relates #39

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -3,7 +3,9 @@ const { postPhoto } = require('./../model/queries/postPhoto');
 exports.get = (req, res, next) => {    
     return postPhoto(req.body.url, req.body.review)
     .then(res.redirect('/'))
-    .catch(console.log);
-
+    .catch((err) => {
+        console.log(err);
+        next(err);
+    });
   next();
 };
