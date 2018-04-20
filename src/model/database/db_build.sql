23242b651659696607f3214e10ca724d639cdbdc
eadehemingway <eade.hemingway@gmail.com>
2018-04-20 12:28:13 +0100

    typo

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -10,8 +10,6 @@ CREATE TABLE photos
 INSERT INTO photos
     (url, caption)
 VALUES
-    ('http://www.theparisreview.org/blog/wp-content/uploads/2016/09/3.jpeg', 'Cy Twombly'),
-    ('https://i.pinimg.com/564x/c1/88/6f/c1886f41c67fbec3fe4ee88c258cce5d.jpg', 'another... '),
     ('http://museubispodorosario.com/wp-content/uploads/2015/03/bispo_01.jpg', 'Arthur Rosario de Bispo'),
     ('https://i.pinimg.com/564x/22/58/ab/2258ab82d5b080026e4ae7ad1456d612.jpg', 'yes matee'),
     ('http://www.museum-brandhorst.de/typo3temp/pics/e2c0fff0a4.jpg', 'Cy again'),
@@ -22,5 +20,7 @@ VALUES
     ('http://museubispodorosario.com/wp-content/uploads/2015/03/bispo_01.jpg', 'Arthur Rosario de Bispo'),
     ('http://www.museum-brandhorst.de/typo3temp/pics/e2c0fff0a4.jpg', 'Cy again'),
     ('https://i.ytimg.com/vi/sgYTlhGc-E8/maxresdefault.jpg', 'Arthur Ganson'),
-    ('https://i.pinimg.com/564x/33/ba/26/33ba265d297950c48159cdf0a41ecf7f.jpg', 'Peter Lanyons');
+    ('https://i.pinimg.com/564x/33/ba/26/33ba265d297950c48159cdf0a41ecf7f.jpg', 'Peter Lanyons'),
+    ('http://www.theparisreview.org/blog/wp-content/uploads/2016/09/3.jpeg', 'Cy Twombly'),
+    ('https://i.pinimg.com/564x/c1/88/6f/c1886f41c67fbec3fe4ee88c258cce5d.jpg', 'another... ');
 COMMIT;
