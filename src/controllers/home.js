abb5d9f0f0f9ca9619ed45612ba8fdcc3fa9c1a9
tdoran <tomgdoran@gmail.com>
2018-04-22 10:20:53 +0100

    added fade in upon post Relates #29

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,12 +3,20 @@ const cookieSession = require('cookie-session');
 
 exports.get = (req, res, next) => {
   req.session.loggedin = true;
+  console.log(req.session);
   return getAllPhotos()
     .then(photoData => {
       if (photoData) {
         photoData.reverse();
       }
-      res.render('home', { photoData });
+      if(req.session.justposted){
+        req.session.justposted = false;
+        let fadeInClass = ['fade-in'];
+        let firstPhoto = photoData.shift();
+      res.render('home', { photoData, firstPhoto, fadeInClass });
+      } else { 
+        res.render('home', { photoData });
+      }
     })
     .catch(err => {
       console.log(err);
